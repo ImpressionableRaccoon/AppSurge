12ca76569d77ac92cf20ec372b548589f8aeaafb
Vadim Vladimirov <impressionableracoon@gmail.com>
2023-06-03 16:36:37 +0300

    Initial Commit

---
+++ AppSurge/AppSurgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSurgeApp.swift
+//  AppSurge
+//
+//  Created by Vadim Vladimirov on 03.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppSurgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
